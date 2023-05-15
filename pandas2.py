27155fc4def1f1202b165006d218eecb25ce7b04
anirudhkaluri <anirudhkaluri@gmail.com>
2023-05-15 12:39:29 +0530

    read json

--- pandas2.py
+++ pandas2.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
 df=pd.read_csv('data.csv')
-data=df.to_string()
+#print first few rows
 print(df.head())
+
+#lets see how many maxiumum rows by default pandas displays
+print(pd.options.display.max_rows)
+#lets change it
+pd.options.display.max_rows=98
+
+#lets try to read json file
+data=pd.read_json('data.json')
+df=pd.DataFrame(data)
+print(df)
